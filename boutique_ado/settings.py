73d1e2ffa5a6186496864788ed58fbba7814f5ca
paulfitzpatrick85 <fitzer99@gmail.com>
2022-09-10 20:23:17 +0000

    complete homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -150,6 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # tells django where all static files are located
+
+
+MEDIA_URL = '/media/'             
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    #where all uploaded media fileswill go
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
