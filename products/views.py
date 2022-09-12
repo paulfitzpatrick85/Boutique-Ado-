e275a1fb9addc2b12a1093bdcb03de5ec209df28
paulfitzpatrick85 <fitzer99@gmail.com>
2022-09-12 21:28:02 +0000

    3 min mark of product detail vid

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -8,6 +8,19 @@ def all_products(request):
 
     context = {              # make products available in the template
         'products': products,
+    }
+
+    return render(request, 'products/products.html', context)  
+    # context is needed to send things back to the template
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    products = get_object_or_404(Product, pk=product_id)
+
+    context = {              # make products available in the template
+        'products': product,
     }
 
     return render(request, 'products/products.html', context)  
