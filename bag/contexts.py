9864b5ff6dabdfa15f8d26a88f502586f3ccabce
paulfitzpatrick85 <fitzer99@gmail.com>
2022-09-25 10:50:21 +0000

    adding products part 4

--- bag/contexts.py
+++ bag/contexts.py
@@ -8,19 +8,32 @@ def bag_contents(request):
     bag_items = []
     total = 0        # intialize to zero
     product_count = 0
-    bag = request.session.get('bag', {})           #access bag in session, session allows info to be stored until client and server are done communicating
-                                                     #gets bag if it exists or intialize to empty dict if not
+    bag = request.session.get('bag', {})           # access bag in session, session allows info to be stored until client and server are done communicating
+                                                     # gets bag if it exists or intialize to empty dict if not
 
     # iterate through items in bag and get quantity and cost and add to bag_items array
-    for item_id, quantity in bag.items():                   #bag from session
-        product = get_object_or_404(Product, pk=item_id)     #get the product
-        total += quantity * product.price                     # add quantity times the price to the total
-        product_count += quantity                     # increment product count by quantity
-        bag_items.append({                          # add dict to list of bag item
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,            # adding product object give access to all its related info when iterating through bag items in templates
-        })
+    for item_id, item_data in bag.items():                   # bag from session, item with no size-item will just be quantity, otherwise will be dict of item by size
+       # only use this code if no sizes, evident by checking if item is int 
+        if isinstance(item_data, int):                    # if is int, then item data = quantity, otherwise its a dict
+            product = get_object_or_404(Product, pk=item_id)     # get the product
+            total += item_data * product.price                     # add quantity times the price to the total
+            product_count += item_data                     # increment product count by quantity
+            bag_items.append({                          # add dict to list of bag item
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,            # adding product object give access to all its related info when iterating through bag items in templates
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items(): # iterate through inner dict by items_by_size
+                total += quantity * product.price  
+                product_count += quantity         # increment product and total
+                bag_items.append({   #for each item, add size to bag items returned to the template, this is how to render the sizes in the template
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
--- bag/views.py
+++ bag/views.py
@@ -12,14 +12,29 @@ def add_to_bag(request, item_id):  # id of product being added to bag
 
     quantity = int(request.POST.get('quantity'))       # get quatity from form. will come from template as string, so convert to int
     redirect_url = request.POST.get('redirect_url')   # where to redirect once process is finished
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})             # session allows info to be stored until client and server are done communicating
                                                     # in the case of e-commerce, allows cart items to be stored while user browses website
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity    #if item already in bag/key already in dict mathcing product id, then increment quantity
-    else:
-        bag[item_id] = quantity       # set key of item id to quantity
+    
+    if size:           # check if prod with size being added
+        if item_id in list(bag.keys()):                     # if item in bag
+            if size in bag[item_id]['items_by_size'].keys(): # check if item of same id and size already exists
+                bag[item_id]['items_by_size'][size] += quantity # if so, increment item for that size
+            else:
+                bag[item_id]['items_by_size'][size] = quantity # otherwise, if new size for same item, set to quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}} # if not in bag, add it. by size used over id in case same item different size is bought
+    
+    else: # if no size, run logic below
+    
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity    # if item already in bag/key already in dict mathcing product id, then increment quantity
+        else:
+            bag[item_id] = quantity       # set key of item id to quantity
 
     request.session['bag'] = bag          # put 'bag' variable into session: overwrite variable in session with updated version
-   # print(request.session['bag'])         #just for testing that products are adding to, only seen in terminal though
+   #  print(request.session['bag'])         #just for testing that products are adding to, only seen in terminal though
     return redirect(redirect_url)   # redirect user back to redirect url
     
